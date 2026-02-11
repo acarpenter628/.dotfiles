741a0f2fde0ce13edd2048194c290a6994e92c5b
Austin Carpenter <austin.carpenter@intusurg.com>
2026-02-11 11:31:51 -0500

    Adding script to make symlinks.  Seemed to work on my home laptop, haven't tested at work yet.  Fingies crossed

---
+++ make-symlinks.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+############################
+# .make.sh
+# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
+############################
+
+########## Variables
+
+dir=~/.dotfiles                    # dotfiles directory
+olddir=~/.dotfiles_backup             # old dotfiles backup directory
+files="bashrc bash_alias config/nvim config/ranger tmux.conf"    # list of files/folders to symlink in homedir
+
+##########
+
+# create olddir in homedir
+echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
+mkdir -p $olddir
+echo "done" 
+
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
+for file in $files; do
+   echo "Moving any existing dotfiles from ~ to $olddir"
+   mv ~/.$file $olddir
+   echo "Creating symlink to $file in home directory."
+   ln -s $dir/.$file ~/.$file
+done
