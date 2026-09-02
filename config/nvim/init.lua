60ca73648481505e0d9abcae1787875d932577d3
Austin Carpenter <austin.carpenter@intusurg.com>
2026-09-02 15:20:48 -0400

    add diff view even though it doesn't work on the work computer bc git is too old.  add another new tab mapping to zellij

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -429,6 +429,7 @@ rtp:prepend(lazypath)
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
+  'sindrets/diffview.nvim',
   {
     'nat-418/boole.nvim',
     config = function()
