0d0ed54df116acc354852a2769f105ac3854ea15
Austin Carpenter <austin.carpenter@intusurg.com>
2026-09-07 12:10:24 -0400

    fix ~ in script oops

--- make-symlinks.sh
+++ make-symlinks.sh
@@ -6,8 +6,8 @@
 
 ########## Variables
 
-dir=~/.dotfiles                    # dotfiles directory
-olddir=~/.backup_dotfiles             # old dotfiles backup directory
+dir=$HOME/.dotfiles                    # dotfiles directory
+olddir=$HOME/.backup_dotfiles             # old dotfiles backup directory
 # list of files/folders to symlink in homedir
 files="bashrc bash_aliases"
 dirs="config/nvim config/ranger config/zellij config/wezterm"
@@ -15,26 +15,26 @@ dirs="config/nvim config/ranger config/zellij config/wezterm"
 ##########
 
 # create olddir in homedir
-echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
+echo -n "Creating $olddir for backup of any existing dotfiles in $HOME ..."
 mkdir -p $olddir
 echo "done" 
 
-mkdir -p "~/.config"
+mkdir -p "$HOME/.config"
 
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the $HOME/dotfiles directory specified in $files
 for file in $files; do
    touch .$file
-   echo "Moving .$file from ~ to $olddir"
-   #mkdir -p ~/.$file # only for directories, this breaks .bashrc.  split the files list
-   mv ~/.$file $olddir
+   echo "Moving .$file from $HOME to $olddir"
+   #mkdir -p $HOME/.$file # only for directories, this breaks .bashrc.  split the files list
+   mv $HOME/.$file $olddir
    echo "Creating symlink to $file in home directory."
-   ln -s $dir/$file ~/.$file
+   ln -s $dir/$file $HOME/.$file
 done
 
 for file in $dirs; do
-   echo "Moving .$file from ~ to $olddir"
+   echo "Moving .$file from $HOME to $olddir"
    mkdir -p .$file
-   mv ~/.$file $olddir
+   mv $HOME/.$file $olddir
    echo "Creating symlink to $file in home directory."
-   ln -s $dir/$file ~/.$file
+   ln -s $dir/$file $HOME/.$file
 done
